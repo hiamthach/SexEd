f2692ba239905a0d8a4988237392e3fcc73f2077
Ngoc Thach <thach.le@topebox.com>
2022-12-29 09:45:29 +0700

    next config

--- next.config.js
+++ next.config.js
@@ -3,10 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
 };
 
-const nextTranslate = require('next-translate');
-
 module.exports = {
-  ...nextTranslate(),
   images: {
     domains: ['firebasestorage.googleapis.com', 'topebox-news.appspot.com', 'www.topebox.com'],
   },
